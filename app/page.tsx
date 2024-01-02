715810ff40e20c6241181da109383846d576a4d5
Shimadakunn <lcombaret@hotmail.co.jp>
2024-01-02 18:38:00 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 "use client";
 import { useRouter, usePathname } from "next/navigation";
+import { useState,useEffect } from "react";
 import Scrumble from "@/components/ui/scrumble-text/scrumble";
 import { Button } from "@/components/ui/button";
 
+import { Web5 } from "@web5/api";
+
 const MyPage = () => {
   const router = useRouter();
+
+  const [web5, setWeb5] = useState<Web5>();
+  const [myDid, setMyDid] = useState<string>('');
+
+  useEffect(() => {
+    const connectWeb5 = async () => {
+      const web5Result = await Web5.connect();
+      setWeb5(web5Result.web5);
+      setMyDid(web5Result.did);
+    }
+    connectWeb5();
+  }, []);
+  
   return (
     <main className="bg-[url('../public/background.png')] bg-cover flex items-center justify-center w-screen h-screen space-y-4">
       <div className="w-full h-full flex flex-col items-left justify-center p-32">
@@ -23,6 +39,7 @@ const MyPage = () => {
       </Button>
       </div>
       <div className="w-full">
+      {myDid}
       </div>
     </main>
   
